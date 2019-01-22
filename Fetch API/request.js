69e97afa282a60f3131a3c6238207bcdc1c7ac38
Murat Güven <muratguven_ktu@hotmail.com>
2019-01-23 00:15:16 +0300

    fetch api post method

--- Fetch API/request.js
+++ Fetch API/request.js
@@ -11,9 +11,30 @@ class Request {
         
     }
 
+
+    post(url,data){
+
+        return new Promise((resolve,reject)=>{
+            fetch(url,{
+                method:"POST",
+                body:JSON.stringify(data),
+                headers:{"Content-type": "application/json; charset=UTF-8"}
+            })
+            .then(response=>response.json())
+            .then(data=>resolve(data))
+            .catch(err=>reject(err));
+
+        });
+       
+    }
+
 }
 
+// Get Test
+const req = new Request();
+// req.get("https://jsonplaceholder.typicode.com/albums").then(response=>console.log(response));
 
-const getRequest = new Request();
-getRequest.get("https://jsonplaceholder.typicode.com/albums").then(response=>console.log(response));
+// Post Test 
+req.post("https://jsonplaceholder.typicode.com/albums",{userId:1,title:"DENEME"}).then(result=>console.log(result));
+
 
