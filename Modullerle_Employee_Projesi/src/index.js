1299a15ad0007e1465c5187714ee9594d9aaa0c6
Murat Güven <muratguven_ktu@hotmail.com>
2019-02-10 15:16:53 +0300

    request class

--- Modullerle_Employee_Projesi/src/index.js
+++ Modullerle_Employee_Projesi/src/index.js
@@ -1,1 +1,20 @@
-console.log("Test");
+import {Request} from "./request";
+import {UI} from "./ui";
+
+const form  = document.getElementById("employee-form");
+const nameInput  = document.getElementById("name");
+const departmentInput  = document.getElementById("department");
+const salaryInput  = document.getElementById("salary");
+const employeeList  = document.getElementById("employees");
+const updateEmployeeButton  = document.getElementById("update");
+
+eventListerners();
+
+function eventListerners(){
+    document.addEventListener("DOMContentLoaded", getAllEmployees);
+}
+
+function getAllEmployees(){
+
+    
+}
---
+++ Modullerle_Employee_Projesi/src/ui.js
@@ -0,0 +1,13 @@
+export class UI {
+
+    constructor() {
+        this.nameInput = document.getElementById("name");
+        this.departmentInput = document.getElementById("department");
+        this.salaryInput = document.getElementById("salary");
+        this.employeeList = document.getElementById("employees");
+        this.updateEmployeeButton = document.getElementById("update");
+    }
+
+    
+
+}
