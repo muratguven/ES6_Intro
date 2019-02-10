6c599288686ca549a288ed4cd33547d06b13b69f
Murat Güven <muratguven_ktu@hotmail.com>
2019-02-11 00:11:46 +0300

    add employee and employee list

--- Modullerle_Employee_Projesi/src/index.js
+++ Modullerle_Employee_Projesi/src/index.js
@@ -7,14 +7,47 @@ const departmentInput  = document.getElementById("department");
 const salaryInput  = document.getElementById("salary");
 const employeeList  = document.getElementById("employees");
 const updateEmployeeButton  = document.getElementById("update");
+const request = new Request("http://localhost:3000/employees");
+const ui = new UI();
+
 
 eventListerners();
 
 function eventListerners(){
     document.addEventListener("DOMContentLoaded", getAllEmployees);
+    form.addEventListener("submit", addEmployee);
+    employeeList.addEventListener("click", deleteEmployee);
 }
 
 function getAllEmployees(){
-
+     request.get()
+        .then(employees=>{
+            ui.addAllEmployeesToUi(employees);
+        })
+        .catch(err=>console.log(err));
     
 }
+
+
+function addEmployee(e){
+
+    const employeeName = nameInput.value.trim();
+    const employeeDepartment = departmentInput.value.trim();
+    const employeeSalary = salaryInput.value.trim();
+    if(employeeName==""||employeeDepartment==""||employeeSalary==""){
+        alert("Lütfen alanları tam doldurun....");
+        
+    }else{
+        request.post({name:employeeName,department:employeeDepartment, salary:Number(employeeSalary)})
+        .then(employee=>{
+            ui.addEmployeesToUi(employee);
+        })
+        .catch(err=>console.log(err));
+
+    }
+    
+    ui.clearInputs();
+    e.preventDefault();
+}
+
+
--- Modullerle_Employee_Projesi/src/ui.js
+++ Modullerle_Employee_Projesi/src/ui.js
@@ -8,6 +8,38 @@ export class UI {
         this.updateEmployeeButton = document.getElementById("update");
     }
 
-    
+    addAllEmployeesToUi(employees){
+        let result = "";
 
+        employees.forEach(item => {
+            result += `<tr>
+                                            
+            <td>${item.name}</td>
+            <td>${item.department}</td>
+            <td>${item.salary}</td>
+            <td>${item.id}</td>
+            <td><a href="#" id = "update-employee" class= "btn btn-danger">Güncelle</a></td> 
+            <td> <a href="#" id = "delete-employee" class= "btn btn-danger">Sil</a></td>
+        </tr>`;
+        });
+        this.employeeList.innerHTML=result;
+    }
+
+    clearInputs(){
+        this.nameInput.value ="";
+        this.departmentInput.value ="";
+        this.salaryInput.value ="";
+    }
+
+    addEmployeeToUI(employee){
+        this.employeeList.innerHTML += `<tr>
+                                            
+        <td>${employee.name}</td>
+        <td>${employee.department}</td>
+        <td>${employee.salary}</td>
+        <td>${employee.id}</td>
+        <td><a href="#" id = "update-employee" class= "btn btn-danger">Güncelle</a></td> 
+        <td> <a href="#" id = "delete-employee" class= "btn btn-danger">Sil</a></td>
+    </tr>`;
+    }
 }
